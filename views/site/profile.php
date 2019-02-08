703b998edb1ea3128d392a5419762262eaf3893d
lu1tr0n <lu1tr0n.gh0st@gmail.com>
2019-02-08 08:00:25 -0600

    Se agrego campos con datos del usuario

--- views/site/profile.php
+++ views/site/profile.php
@@ -1,38 +1,55 @@
+<?php
+
+/* @var $this \yii\web\View */
+/* @var $content string */
+
+use app\widgets\Alert;
+use yii\helpers\Html;
+use yii\bootstrap\Nav;
+use yii\bootstrap\NavBar;
+use yii\widgets\Breadcrumbs;
+use yii\bootstrap\ActiveForm;
+use yii\helpers\Url;
+
+?>
+
 <div class="container">
   <div class="row">
     <div class="col"></div>
     <div class="col-6">
-    <div class="input-group mb-3">
-        <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon3">Name</span>
-            </div>
-            <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3" value="<?php echo $data['name'] ?>">
-        </div>
-
-        <div class="input-group mb-3">
+        <form id="frm-profile" action="<?php echo Url::to(['site/updateprofile'], true); ?>" method="POST">
+            <div class="input-group mb-3">
             <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon3">Username</span>
+                    <span class="input-group-text" id="basic-addon3">Name</span>
+                </div>
+                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3" value="<?php echo $data['name'] ?>">
             </div>
-            <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3" value="<?php echo $data['username']; ?>">
-        </div> 
 
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon3">Password</span>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon3">Username</span>
+                </div>
+                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3" value="<?php echo $data['username']; ?>">
+            </div> 
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon3">Password</span>
+                </div>
+                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
+            </div>         
+
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon3">Email</span>
+                </div>
+                <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3" value="<?php echo $data['email']; ?>">
             </div>
-            <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3">
-        </div>         
 
-        <div class="input-group mb-3">
-            <div class="input-group-prepend">
-                <span class="input-group-text" id="basic-addon3">Email</span>
-            </div>
-            <input type="text" class="form-control" id="basic-url" aria-describedby="basic-addon3" value="<?php echo $data['email']; ?>">
-        </div>
-
-        <div class="input-group m3">
-            <button type="button" class="btn btn-primary">Update profile</button> 
-        </div>
+            <div class="input-group m3">
+                <button type="submit" class="btn btn-primary">Update profile</button> 
+            </div>    
+        </form>
     </div>
     <div class="col"></div>
   </div>
